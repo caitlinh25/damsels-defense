71f9f8a35f90c0adad52017f208be61b6ea987ea
2122000 <michellel11@nyclabschool.org>
2017-08-09 15:41:18 -0700

    more code

--- damsels_and_defense.js
+++ damsels_and_defense.js
@@ -1,22 +1,22 @@
 var button;
-var title;
 
-function intro(){
-  function setup(){
-    canvas = createCanvas(1345,640);
-    background(45);
-    title = "Damsels & Defense";
-   	textFont("VT323");
-    textSize(80);
-    fill(255,255,255);
-    text(title, 450, 321);
-    button = createButton("Play");
-  	button.position(677,400);
-    button.size(100,40);
-  	button.mousePressed(stats);
-    sound = loadSound("damsels_and_defense_music.mp3");
-    sound.loop();
-  }
+function setup(){
+  canvas = createCanvas(1345,640);
+  background(45);
+
+  title = "Damsels & Defense";
+ 	textFont("VT323");
+  textSize(80);
+  fill(255,255,255);
+  text(title, 450, 321);
+
+  button = createButton("Play");
+	button.position(677,400);
+  button.size(100,40);
+	button.mousePressed(stats);
+  // sound = loadSound("damsels_and_defense_music.mp3");
+  // sound.loop();
+}
 
 // function loaded(){
 // 	song.play();
@@ -33,17 +33,28 @@ function stats(){
   rect(30, 40, 1277, 571);
 
   info1 = " Every 98 seconds, an American \n is sexually assaulted and every \n 8 minutes that person is a child."
+  textAlign("center")
   fill(0,0,0);
-  // info2 = " However, only 6 out of every 1000 perpetrators end in jail."
-  // info3 = " In these assaults, 90% of the victims tend to be female while the other 10% is male."
-  // info4 = " Statics have shown that about 55% of the times these assaults happened either at or near the victim's house."
-  // info5 = " Lastly, one of the most under-reported crime is rape as over 63% sexual assaults are not reported to the police."
-  text(info1,50, 100);
+  text(info1,650, 230);
 
+  // info2 = " However, only 6 out of every \n 1000 perpetrators end in jail."
+  // textAlign("center")
+  // fill(0,0,0);
+  // text(info1,650, 230);
+  //
+  // info3 = " In these assaults, 90% of \n the victims tend to be female while \n the other 10% is male."
+  // textAlign("center")
+  // fill(0,0,0);
+  // text(info1,650, 230);
+  //
+  // info4 = " Statics have shown that \n about 55% of the times these assaults \n happened either at or near the \n victim's house."
+  // textAlign("center")
+  // fill(0,0,0);
+  // text(info1,650, 230);
+  //
+  // info5 = " Lastly, one of the most \n under-reported crime is rape as over \n 63% sexual assaults are not \n reported to the police."
+  // textAlign("center")
+  // fill(0,0,0);
+  // text(info1,650, 230);
 
-
-  // text(info2,50, 50);
-  // text(info3,50, 50);
-  // text(info4,50, 50);
-  // text(info5,50, 50);
 }
